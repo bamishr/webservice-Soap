c986b21e8df81b43542025d96f157b9232318df7
badrish mishra <badrish.kumar.mishra@gmail.com>
2020-11-18 09:23:11 +0000

    changes in persistence.xml

--- src/main/java/com/pauloneto/webservicejee/ws/IPerfilWs.java
+++ src/main/java/com/pauloneto/webservicejee/ws/IPerfilWs.java
@@ -21,3 +21,8 @@ public interface IPerfilWs {
 	@WebMethod
 	public List<PerfilDTO> obterTodosPerfis();
 }
+public interface IPerfilWs {
+
+	@WebMethod
+	public List<PerfilDTO> obterTodosPerfis();
+}
